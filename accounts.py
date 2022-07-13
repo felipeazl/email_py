629dd0905ae98cf9c13e38885dda7fadf87c2b1d
felipeazl <felipelouzada671@gmail.com>
2022-07-13 09:50:45 -0300

    .

---
+++ accounts.py
@@ -0,0 +1,9 @@
+class EmailAccounts:
+    def __init__(self, nome, email, senha):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+
+item = EmailAccounts("Felipe", "felipe@domain.com", "senha")
+list = [item]
